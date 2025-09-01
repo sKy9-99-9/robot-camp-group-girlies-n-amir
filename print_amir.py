ec21f613e51a598d992a351a1d53b1c07a1c6bfb
alissamsh <alissa.mishima.uva@gmail.com>
2025-09-01 13:15:19 +0200

    fix typos amir

--- print_amir.py
+++ print_amir.py
@@ -12,7 +12,7 @@ def act1_amir():
     from print_mathilde import char_mati
     return (
         f"WooooWooWooWoo {char_laur()} WooWoWOOOWoooWooWoo. WooooWoWooWOoo {char_pix()} WOOWoooWooWooWoo. "
-        f"WOWO      OWOooWooWoo {char_ali()} WooooWoooooooWooWooWooWoo. WOWoWooWooWoo {char_mati()} WooooooWOOooowooWooWoooWoo. "
+        f"WOWOOWOooWooWoo {char_ali()} WooooWoooooooWooWooWooWoo. WOWoWooWooWoo {char_mati()} WooooooWOOooowooWooWoooWoo. "
         f"WooWooWooooWOOO {char_laur()} WooWooooWooWooWoWooWooWoo. WoooWooWooo {char_pix()} WooWOOoWooWooWooWoo. "
         f"WoooWooooWoo {char_ali()} WooooWooWooWooWooWooWoo. WooWooWooWooo {char_mati()} WooooWooWooWOOoooWooWooWooo."
     )
@@ -25,7 +25,7 @@ def act2_amir():
     return (
         f"Wooo WooWooWoo {char_ali()} Woooo Woooooo WooWoo. WooWoo WOOoo {char_mati()} Wooo WooWooWoo WooWoo. "
         f"WooooWoo W {char_laur()} Woo WoooWooWOO ooo. WoooWOOwooo {char_pix()} WooWooWooo Woo Woo. "
-        f"WooWooWoo {char_ali()} WooWooWoo WooWooZap. Woooo Woo {char_mati()} Woo Woo WooWoo. "
+        f"WooWooWoo {char_ali()} WooWooWoo WooWooWoo. Woooo Woo {char_mati()} Woo Woo WooWoo. "
         f"Woo WooWoo {char_laur()} WooWooWoo Woo Woo. WooooWooooo {char_pix()} Woo WooWooWoo."
     )
 
@@ -35,7 +35,7 @@ def act3_amir():
     from print_alissa import char_ali
     from print_mathilde import char_mati
     return (
-        f"WoooWoo Woo {char_ali()} WooWooWooWoooo plop. WooWoo WOOOo {char_mati()} WoooWoo WooWooWoo. "
+        f"WoooWoo Woo {char_ali()} WooWooWooWooooWooo. WooWoo WOOOo {char_mati()} WoooWoo WooWooWoo. "
         f"Woo WooWoo {char_laur()} WooooWOO Woo. WooooWooWoo {char_pix()} WooWoo WooWoo Woo. "
         f"WooWooWooWoo {char_ali()} Woo WooWoop. Wooo Woo {char_mati()} WooWoo WoooWoo. "
         f"WooWoo Wooo {char_laur()} Woo WooWoo Woo. WooWooWooooo {char_pix()} WooWooWoo Woo."
