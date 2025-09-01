ade5775e0eb4de0ac6ed273dd53c60bbdc549e17
Lauraponoran <lauraponoran7@gmail.com>
2025-09-01 13:13:37 +0200

    Fixed typos

--- print_amir.py
+++ print_amir.py
@@ -14,7 +14,7 @@ def act1_amir():
         f"WooooWooWooWoo {char_laur()} WooWoWOOOWoooWooWoo. WooooWoWooWOoo {char_pix()} WOOWoooWooWooWoo. "
         f"WOWO      OWOooWooWoo {char_ali()} WooooWoooooooWooWooWooWoo. WOWoWooWooWoo {char_mati()} WooooooWOOooowooWooWoooWoo. "
         f"WooWooWooooWOOO {char_laur()} WooWooooWooWooWoWooWooWoo. WoooWooWooo {char_pix()} WooWOOoWooWooWooWoo. "
-        f"WoooWooooWoo {char_ali()} WooooWooWooWooWooWooWoo. WooWooWooWooo {char_mati()} WooooWooWooWOOoooWooWooWooo. Waht"
+        f"WoooWooooWoo {char_ali()} WooooWooWooWooWooWooWoo. WooWooWooWooo {char_mati()} WooooWooWooWOOoooWooWooWooo."
     )
 
 def act2_amir():
@@ -23,8 +23,8 @@ def act2_amir():
     from print_alissa import char_ali
     from print_mathilde import char_mati
     return (
-        f"Wooo WooWooWoo {char_ali()} Woooo Woooooo WooWoo Bzz. WooWoo WOOoo {char_mati()} Wooo WooWooWoo WooWoo. "
-        f"WooooWoo W {char_laur()} Woo WoooWooWOO ooo. WoooWOOwooo {char_pix()} WooWooWooo blip Woo Woo. "
+        f"Wooo WooWooWoo {char_ali()} Woooo Woooooo WooWoo. WooWoo WOOoo {char_mati()} Wooo WooWooWoo WooWoo. "
+        f"WooooWoo W {char_laur()} Woo WoooWooWOO ooo. WoooWOOwooo {char_pix()} WooWooWooo Woo Woo. "
         f"WooWooWoo {char_ali()} WooWooWoo WooWooZap. Woooo Woo {char_mati()} Woo Woo WooWoo. "
         f"Woo WooWoo {char_laur()} WooWooWoo Woo Woo. WooooWooooo {char_pix()} Woo WooWooWoo."
     )
@@ -36,8 +36,8 @@ def act3_amir():
     from print_mathilde import char_mati
     return (
         f"WoooWoo Woo {char_ali()} WooWooWooWoooo plop. WooWoo WOOOo {char_mati()} WoooWoo WooWooWoo. "
-        f"Woo WooWoo {char_laur()} WooooWOO zzz Woo. WooooWooWoo {char_pix()} WooWoo blip WooWoo Woo. "
-        f"WooWooWooWoo {char_ali()} Woo WooWooZapZap. Wooo Woo {char_mati()} WooWoo WoooWoo. "
-        f"WooWoo Wooo {char_laur()} Woo WooWoo Woo. WooWooWooooo {char_pix()} WooWooWoo Woo err."
+        f"Woo WooWoo {char_laur()} WooooWOO Woo. WooooWooWoo {char_pix()} WooWoo WooWoo Woo. "
+        f"WooWooWooWoo {char_ali()} Woo WooWoop. Wooo Woo {char_mati()} WooWoo WoooWoo. "
+        f"WooWoo Wooo {char_laur()} Woo WooWoo Woo. WooWooWooooo {char_pix()} WooWooWoo Woo."
     )
 
