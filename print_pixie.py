0e5c7d1386ef046008725fe4c7b5d50d75e95d24
Lauraponoran <lauraponoran7@gmail.com>
2025-09-01 13:18:53 +0200

    Fixed typos in print_pixie.py

--- print_pixie.py
+++ print_pixie.py
@@ -12,7 +12,7 @@ def act1_pixie():
     return (
         f"{char_pix()} arrived late at her appointment, as they always are. {char_laur()} was mad but luckily {char_pix()} still had some pixie dust collected the night before from the starry sky. "
         "A sprinkle of magic powder had Staurt hallucinating, walls were not straight anymore and chandeliers were falling from the ceiling. "
-        f"When {char_ali()} got to the studio {char_laur()} had disappeared so since she was busy she quickly left to go to her 17th appointment of the day after refusing {char_pix()}’s pixie dust as she had to drive and was scarred for her drivers license to be taken away. "
+        f"When {char_ali()} got to the studio {char_laur()} had disappeared so since she was busy she quickly left to go to her 17th appointment of the day after refusing {char_pix()}’s pixie dust as she had to drive and was scared for her drivers license to be taken away. "
         f"{char_amr()} was next but they had to wait for {char_mati()} to get there since she was the only one who could understand him given her powers. {char_pix()} kept chilling in a corner enjoying the company of her atypical friends."
     )
 
@@ -24,7 +24,7 @@ def act2_pixie():
     return (
         f"{char_pix()} was shocked. {char_laur()} had a wife? They all stared at him in silence for a minute. Even {char_amr()} was quiet for a change. "
         f"{char_mati()} had to do no translation anymore, her power was useless and she felt lost without it. {char_ali()} revived the mushroom that had gotten wrinkly from absorbing all of the sadness in the surrounding area. "
-        f"{char_pix()} asked the sun (still a star at the end of the day) to play some music in the baqround and Sabrina Woodworker filled the air. The mood lightened up in a second."
+        f"{char_pix()} asked the sun (still a star at the end of the day) to play some music in the background and Sabrina Woodworker filled the air. The mood lightened up in a second."
     )
 
 def act3_pixie():
@@ -35,7 +35,7 @@ def act3_pixie():
     return (
         f"While they all waited for the lasagna to cook in the oven {char_laur()} found {char_pix()}’s crystal ball and brought it over to the table. "
         f"{char_ali()} and {char_mati()} looked into it and flickering at the bottom of it the truth came into sight. Their eyes unlocked and filled with tears. "
-        "Having spent their whole life looking for their long lost twins and finally being reunited filed their hearts with joy. "
+        "Having spent their whole life looking for their long lost twins and finally being reunited filled their hearts with joy. "
         f"Star dust fell from the sky and the bright moon lighted the room, making this a moment {char_amr()}, even during his long life would never forget."
     )
 
