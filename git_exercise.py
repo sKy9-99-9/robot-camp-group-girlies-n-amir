aa2da31db8059c542d049a4b53b00aacd85d7bd3
pixie-pic <saranunziatini27@gmail.com>
2025-09-01 10:19:24 +0200

    file that prints the group names

---
+++ git_exercise.py
@@ -0,0 +1,22 @@
+from print_pixie import get_name
+from print_laura import print_laura
+from print_amir import mfut
+from print_alissa import print_alissa
+from print_mathilde import print_alissa
+from intro import get_intro
+
+def get_group_names():
+
+  intro = get_intro()
+  
+  names = [
+        get_name(),
+        print_laura(), 
+        mfut(),
+        print_alissa(),
+        mathilde_name()
+  ]
+    
+  return intro + ", ".join(names)
+
+print(get_group_names)
