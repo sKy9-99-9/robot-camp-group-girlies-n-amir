cec6df684c689c6e8c0889274bd25b947a66336a
sKy9-99-9 <76662409+sKy9-99-9@users.noreply.github.com>
2025-09-02 09:45:09 +0200

    Prime_Station_4

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -1,1 +1,2 @@
-def solution_station_3()
+def solution_station_3():
+    return
--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -1,3 +1,24 @@
-def solution_station_4(input):
-  output=""
-  return output
+def solution_station_4(n: int) -> bool:
+    try:
+        
+        if n <= 1:
+            print(False)
+            return
+        if n <= 3:
+            print(True)
+            return
+        if n % 2 == 0 or n % 3 == 0:
+            print(False)
+            return
+        i = 5
+        while i * i <= n:
+            if n % i == 0 or n % (i + 2) == 0:
+                print(False)
+                return
+            i += 6
+        print(True)
+    except Exception:
+        print(False)
+
+n = int(input().strip())
+solution_station_4(n)
