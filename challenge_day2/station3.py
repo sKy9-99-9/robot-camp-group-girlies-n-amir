31551bdbe21e5e02a8fe986cb16bfbd2de773a86
sKy9-99-9 <76662409+sKy9-99-9@users.noreply.github.com>
2025-09-02 09:56:49 +0200

    solution for 1

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -1,2 +1,7 @@
-def solution_station_3():
-    return
+def solution_station_3(n: int) -> bool:
+    if n%3==0:
+        return True
+    return False
+
+n = int(input().strip())
+print(solution_station_3(n))
--- challenge_day2/station4.py
+++ challenge_day2/station4.py
@@ -2,23 +2,19 @@ def solution_station_4(n: int) -> bool:
     try:
         
         if n <= 1:
-            print(False)
-            return
+            return False
         if n <= 3:
-            print(True)
-            return
+            return True
         if n % 2 == 0 or n % 3 == 0:
-            print(False)
-            return
+            return False
         i = 5
         while i * i <= n:
             if n % i == 0 or n % (i + 2) == 0:
-                print(False)
-                return
+                return False
             i += 6
-        print(True)
+        return True
     except Exception:
-        print(False)
+        return False
 
 n = int(input().strip())
-solution_station_4(n)
+print(solution_station_4(n))
